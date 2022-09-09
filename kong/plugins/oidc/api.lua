fd70c9ba562972c1068bfab74b11ea30b1a5b46e
Jorgen Nystad <jn@avinet.no>
2022-09-09 15:29:54 +0200

    Amend the admin API

--- kong/plugins/oidc/api.lua
+++ kong/plugins/oidc/api.lua
@@ -1,31 +1,17 @@
 local typedefs = require "kong.db.schema.typedefs"
 
-local schema = {
-  name         = "user_refreshed",
-  workspaceable = true,
-
-  fields = {
-    { timestamp     = typedefs.auto_timestamp_s },
-  },
-}
-
 local shared = ngx.shared
 local store = shared['sessions']
 
 return {
-  ["/oidc/:user/refreshed"] = {
-    schema = schema,
-    methods = {
+  ["/oidc/refresh_user"] = {
+    GET = function(self)
+      return kong.response.exit(200, { timestamp = store["renewal_" .. self.params.user] })
+    end,
 
-      GET = function(self)
-        return { status = 200, json = { timestamp = store["renewal_" .. self.params.user] } }
-      end,
-
-      POST = function(self)
-        store["renewal_" .. self.params.user] = self.params.timestamp
-        return { status = 201, json = { success = true } }
-      end,
-
-    },
+    POST = function(self)
+      store["renewal_" .. self.params.user] = self.params.timestamp
+      return kong.response.exit(201, { success = true })
+    end,
   },
 }
